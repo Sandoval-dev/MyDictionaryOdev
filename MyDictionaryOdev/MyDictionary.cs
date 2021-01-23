89090052d14ad24fa4a070715b029c3ae67af8a3
osman erdem kılıç <OsMaN_eRdEm1200@hotmail.com>
2021-01-23 17:26:27 +0300

    Add project files.

---
+++ MyDictionaryOdev/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryOdev
+{
+    class MyDictionary<K,V>
+    {
+        K[] key;
+        K[] tempkey;
+        V[] value;
+        V[] tempvalue;
+
+        public MyDictionary()
+        {
+            key = new K[0];
+            value = new V[0];
+        }
+
+        public void Add(K _key, V _value)
+        {
+            tempkey = key;
+            tempvalue = value;
+
+            key = new K[key.Length + 1];
+            value = new V[value.Length + 1];
+            for (int i = 0; i < tempkey.Length; i++)
+            {
+                key[i] = tempkey[i];
+            }
+            key[key.Length - 1] = _key;
+
+            for (int i = 0; i < tempvalue.Length; i++)
+            {
+                value[i] = tempvalue[i];
+            }
+            value[value.Length - 1] = _value;
+        }
+
+        public void Yazdir()
+        {
+            for (int i = 0; i < key.Length; i++)
+            {
+                Console.WriteLine("Key: " + key[i] + " Value: " + value[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionaryOdev/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryOdev
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenci = new MyDictionary<int, string>();
+            ogrenci.Add(2, "Almima");
+            ogrenci.Add(3, "Erdem");
+            ogrenci.Add(4, "Osman");
+            ogrenci.Add(5, "Ali");
+
+            ogrenci.Yazdir();
+
+        }
+    }
+}
